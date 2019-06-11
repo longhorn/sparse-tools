f3823aeae8fb88df83c329fd5b1c92fc77860599
Sheng Yang <sheng@yasker.org>
2019-06-11 09:21:19 -0700

    Move repo to `github.com/longhorn/sparse-tools`

--- sfold/main.go
+++ sfold/main.go
@@ -1,6 +1,6 @@
 package main
 
-import "github.com/rancher/sparse-tools/cli/sfold"
+import "github.com/longhorn/sparse-tools/cli/sfold"
 
 func main() {
 	sfold.Main()
--- sparse/test/sfold_test.go
+++ sparse/test/sfold_test.go
@@ -6,7 +6,7 @@ import (
 
 	log "github.com/sirupsen/logrus"
 
-	. "github.com/rancher/sparse-tools/sparse"
+	. "github.com/longhorn/sparse-tools/sparse"
 )
 
 func TestFoldFile1(t *testing.T) {

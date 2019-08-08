05d253328e6d8b0a659b06e04b8026b3eb4d8e03
Michael William Le Nguyen <michael@mail.ttp.codes>
2019-08-08 10:54:41 -0700

    Track progress of sfold operation via callback

--- sparse/test/sfold_test.go
+++ sparse/test/sfold_test.go
@@ -9,6 +9,22 @@ import (
 	. "github.com/longhorn/sparse-tools/sparse"
 )
 
+type FoldFileTest struct {
+	doneChan    chan error
+	progress    int
+	progressErr bool
+}
+
+func (f *FoldFileTest) UpdateFoldFileProgress(progress int, done bool, err error) {
+	if progress < f.progress {
+		f.progressErr = true
+	}
+	f.progress = progress
+	if done {
+		f.doneChan <- err
+	}
+}
+
 func TestFoldFile1(t *testing.T) {
 	// D H D => D D H
 	layoutFrom := []FileInterval{
@@ -158,11 +174,24 @@ func testFoldFile(t *testing.T, layoutFrom, layoutTo []FileInterval) (hashLocal 
 	foldLayout(layoutFrom, layoutTo, fromPath, toPath, expectedPath)
 
 	// Fold
-	err := FoldFile(fromPath, toPath)
-
-	// Verify
+	ops := &FoldFileTest{
+		doneChan: make(chan error),
+	}
+	err := FoldFile(fromPath, toPath, ops)
 	if err != nil {
+		t.Fatal("Start fold error:", err)
+	}
+
+	if err := <-ops.doneChan; err != nil {
 		t.Fatal("Fold error:", err)
+	}
+
+	if ops.progress != 100 {
+		t.Fatal("Completed fold does not have progress of 100")
+	}
+
+	if ops.progressErr {
+		t.Fatal("Progress went backwards during fold")
 	}
 
 	err = checkSparseFiles(toPath, expectedPath)
